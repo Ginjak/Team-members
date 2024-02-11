6c0ac267eb6314cb5503aba5aa96b030ce0b9aa7
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-02-11 15:58:15 +0000

    Added functions to add manager/engineer/itern to team array

--- index.js
+++ index.js
@@ -13,107 +13,187 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const test = new Employee("Gnta", "id2", "email");
-console.log(test.name);
-console.log(test.getId());
+let team = [];
 
-const test2 = new Manager("Manager", "id3", "email", "Officenumber");
-console.log(test2);
-const test3 = new Engineer("engineer", "id", "email", "github");
-console.log(test3);
-console.log(test3.getGithub());
 // Inquirer questions
+const getManagersDetails = () => {
+  const mangerQuestions = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "Team managers name",
+        name: "managersName",
+      },
+      {
+        type: "input",
+        message: "Managers ID",
+        name: "managersID",
+      },
+      {
+        type: "input",
+        message: "Managers email address",
+        name: "managersEmail",
+      },
+      {
+        type: "input",
+        message: "Managers office number",
+        name: "managersNumber",
+      },
+    ]);
+  };
+  async function init() {
+    try {
+      const managerInfo = await mangerQuestions();
+      const manager = new Manager(
+        managerInfo.managersName,
+        managerInfo.managersID,
+        managerInfo.managersEmail,
+        managerInfo.managersNumber
+      );
+      team.push(manager);
+      selectOption();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  init();
+};
+// getManagersDetails();
 
-const mangerQuestions = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "Team managers name",
-      name: "managersName",
-    },
-    {
-      type: "input",
-      message: "Managers ID",
-      name: "managersID",
-    },
-    {
-      type: "input",
-      message: "Managers email address",
-      name: "managersEmail",
-    },
-    {
-      type: "input",
-      message: "Managers office number",
-      name: "managersNumber",
-    },
-  ]);
+const selectOption = () => {
+  const selectAction = () => {
+    return inquirer.prompt([
+      {
+        type: "list",
+        message: "Select your next step",
+        choices: [
+          "Add an engineer",
+          "Add an inter",
+          "Finish building the team",
+        ],
+        name: "options",
+      },
+    ]);
+  };
+  async function init() {
+    try {
+      const options = await selectAction();
+      console.log(options.options);
+      if (options.options === "Add an engineer") {
+        addNewEngineer();
+      } else if (options.options === "Add an inter") {
+        addNewIntern();
+      } else {
+        console.log("Finish app");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  init();
 };
 
-const selectAction = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      message: "Select your next step",
-      choices: ["Add an engineer", "Add an inter", "Finish building the team"],
-      name: "options",
-    },
-  ]);
+selectOption();
+const addNewEngineer = () => {
+  const addEngineer = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "Engineer's name",
+        name: "engineersName",
+      },
+      {
+        type: "input",
+        message: "Engineer's ID",
+        name: "engineersID",
+      },
+      {
+        type: "input",
+        message: "Engineer's email address",
+        name: "engineerEmail",
+      },
+      {
+        type: "input",
+        message: "Engineer's GitHub username",
+        name: "engineerGithub",
+      },
+    ]);
+  };
+  async function init() {
+    try {
+      const engineerInfo = await addEngineer();
+      const engineer = new Engineer(
+        engineerInfo.engineersName,
+        engineerInfo.engineersID,
+        engineerInfo.engineerEmail,
+        engineerInfo.engineerGithub
+      );
+      team.push(engineer);
+      console.log(team);
+      selectOption();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  init();
 };
 
-const addEngineer = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "Engineer's name",
-      name: "engineersName",
-    },
-    {
-      type: "input",
-      message: "Engineer's ID",
-      name: "engineersID",
-    },
-    {
-      type: "input",
-      message: "Engineer's email address",
-      name: "engineerEmail",
-    },
-    {
-      type: "input",
-      message: "Engineer's GitHub username",
-      name: "engineerGithub",
-    },
-  ]);
-};
-
-const addIntern = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "Intern's name",
-      name: "internsName",
-    },
-    {
-      type: "input",
-      message: "Interns's ID",
-      name: "internsID",
-    },
-    {
-      type: "input",
-      message: "Intern's email address",
-      name: "einternsEmail",
-    },
-    {
-      type: "input",
-      message: "Intern's school",
-      name: "internsSchool",
-    },
-  ]);
+// addNewEngineer();
+const addNewIntern = () => {
+  const addIntern = () => {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "Intern's name",
+        name: "internsName",
+      },
+      {
+        type: "input",
+        message: "Interns's ID",
+        name: "internsID",
+      },
+      {
+        type: "input",
+        message: "Intern's email address",
+        name: "einternsEmail",
+      },
+      {
+        type: "input",
+        message: "Intern's school",
+        name: "internsSchool",
+      },
+    ]);
+  };
+  async function init() {
+    try {
+      const internInfo = await addIntern();
+      const intern = new Intern(
+        internInfo.internsName,
+        internInfo.internsID,
+        internInfo.einternsEmail,
+        internInfo.internsSchool
+      );
+      team.push(intern);
+      console.log(team);
+      selectOption();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  init();
 };
 
 async function init() {
   try {
-    const data = await mangerQuestions();
-    console.log(data);
+    const managerInfo = await mangerQuestions();
+    const manager = new Manager(
+      managerInfo.managersName,
+      managerInfo.managersID,
+      managerInfo.managersEmail,
+      managerInfo.managersNumber
+    );
+    team.push(manager);
+    console.log(team);
   } catch (err) {
     console.log(err);
   }
